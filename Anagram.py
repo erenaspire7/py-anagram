1c248b5604e514eb8e950dff43a7944ae047e5f2
Yusuff Jamal <yusuffjamal3@gmail.com>
2023-05-08 13:30:21 +0200

    First Commit

---
+++ Anagram.py
@@ -0,0 +1,36 @@
+import itertools
+
+def generate_anagrams(word):
+    # Convert the word to lowercase and remove spaces
+    word = word.lower().replace(" ", "")
+    
+    # Generate all possible permutations of the letters in the word
+    permutations = itertools.permutations(word)
+
+    # Convert each permutation to a string and check if it's an anagram
+    anagrams = set()
+    for permutation in permutations:
+        permutation_str = "".join(permutation)
+        if permutation_str != word and is_anagram(permutation_str, word):
+            anagrams.add(permutation_str)
+
+    return anagrams
+
+def is_anagram(str1, str2):
+    # Convert strings to lowercase and remove spaces
+    str1 = str1.lower().replace(" ", "")
+    str2 = str2.lower().replace(" ", "")
+
+    # Sort the strings
+    sorted_str1 = sorted(str1)
+    sorted_str2 = sorted(str2)
+
+    # Compare the sorted strings
+    if sorted_str1 == sorted_str2:
+        return True
+    else:
+        return False
+
+# Example usage
+anagrams = generate_anagrams("triangle")
+print(anagrams)
